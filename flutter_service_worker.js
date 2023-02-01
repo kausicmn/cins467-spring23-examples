8ba7a278d93d0562ebfeeb8db0bcb26e4c95a333
Bryan Dixon <javawolfpack@gmail.com>
2023-01-31 16:38:25 -0800

    Testing Branch

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "892f482ee20aa6911451696895e7d6c0",
-"index.html": "6c7b0f2a06a2335eda9b66a1b0dbec5a",
-"/": "6c7b0f2a06a2335eda9b66a1b0dbec5a",
-"main.dart.js": "ec4807ba81fbb574f527b954bc39af66",
-"flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
+"index.html": "d27076f05f6b2e81646a7ef034cdd9af",
+"/": "d27076f05f6b2e81646a7ef034cdd9af",
+"main.dart.js": "0a95ca5bfb4f63c567de590e275e9d82",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
